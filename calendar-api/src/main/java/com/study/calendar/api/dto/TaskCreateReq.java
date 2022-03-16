1c5da1d32c4a4d1f269be83783181112270a62f0
BangDDong <gusehdqkd@naver.com>
2022-03-16 21:47:01 +0900

    Add - Request Validation
    * validation 추가

--- calendar-api/src/main/java/com/study/calendar/api/dto/TaskCreateReq.java
+++ calendar-api/src/main/java/com/study/calendar/api/dto/TaskCreateReq.java
@@ -2,12 +2,18 @@ package com.study.calendar.api.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Data
 public class TaskCreateReq {
 
+    @NotBlank
     private final String title;
+
     private final String description;
+
+    @NotNull
     private final LocalDateTime taskAt;
 }
