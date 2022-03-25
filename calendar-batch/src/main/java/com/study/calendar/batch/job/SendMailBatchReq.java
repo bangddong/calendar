97faa22a9e3ffa344ff937d481b3bd4a5992abf2
BangDDong <gusehdqkd@naver.com>
2022-03-25 18:26:20 +0900

    Add - SendMailBatch
    * 메일 발송 대상 건 조회 로직 추가

---
+++ calendar-batch/src/main/java/com/study/calendar/batch/job/SendMailBatchReq.java
@@ -0,0 +1,19 @@
+package com.study.calendar.batch.job;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Getter
+@Setter
+public class SendMailBatchReq {
+
+    private Long id;
+    private LocalDateTime startAt;
+    private String title;
+    private String userMail;
+
+}
